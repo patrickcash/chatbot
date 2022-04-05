0351c7d682e450e3f878a073723ed693dcbfa667
Patrick Cash <patrickcash@gmail.com>
2022-04-05 13:36:20 -0400

    basic flask app backend to return chatterbot response

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
+chat_bot = ChatBot('ChatBot', storage_adapter='chatterbot.storage.SQLStorageAdapter')
+trainer = ChatterBotCorpusTrainer(chat_bot)
+trainer.train('chatterbot.corpus.english')
+
+
+@app.route('/')
+def index():
+    pass
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    return str(chat_bot.get_response(request.json['message']))
+    
+    
 if __name__ == '__main__':
    app.run(debug=True)
